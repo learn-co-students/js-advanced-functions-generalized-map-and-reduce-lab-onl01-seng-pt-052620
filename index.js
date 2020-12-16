f93328c7ef6c4b33d8f3f8046999b621f2156a7e
8bitbran <brandonmar97@gmail.com>
2020-12-16 12:27:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(arr, callback) {
+    const newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(callback(arr[i]));
+    }
+    return newArr;
+}
+
+function reduce(arr, callback, aggregator) {
+    let r = (!!aggregator) ? aggregator : arr[0];
+    let i = (!!aggregator) ? 0 : 1
+
+    for(; i < arr.length; i++) {
+        r = callback(arr[i], r)
+    }
+    return r;
+}
