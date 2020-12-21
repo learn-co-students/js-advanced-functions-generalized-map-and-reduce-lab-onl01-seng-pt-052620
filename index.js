df9b7b0b9a354cb961e64682c3877d287cdcec39
wynterwolf <shinyguns@gmail.com>
2020-12-21 11:04:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, mapper) {
+    let r = []
+    for (let i = 0; i < array.length; i++) {
+        r.push(mapper(array[i]))
+    }
+    return r
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < src.length; i++) {
+    r = cb(src[i], r)
+    }
+
+    return r;
+}
