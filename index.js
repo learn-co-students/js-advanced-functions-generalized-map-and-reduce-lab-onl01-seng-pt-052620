5eb893ea5f69bc0026972ada4c59d029540ab850
iamcid <iamcid17@gmail.com>
2020-12-30 00:06:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(src, f) {
+    let newArray = []
+    
+    for (let i = 0; i < src.length; i++) {
+        let theElement = src[i]
+        newArray.push(f(theElement))
+    };
+
+    return newArray;
+}
+
+function reduce(src, reduceFn, memo) {
+    
+    src.forEach(item => {
+        if(memo === undefined) {
+            memo =  item
+        } 
+        else {
+            memo = reduceFn(item, memo)
+        }        
+    });
+    return memo;
+}
